ce0ff8dd37160fe8feac48d949894a72eb7dae75
kaktizapisan <sarbeypavel78@gmail.com>
2026-03-07 11:00:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const https = require('https');
 
-const BOT_TOKEN = "8507666775:AAF709GDOtF-EHPrLDgY8ed4QVqAnP6Pt4s";
+const BOT_TOKEN = "8691480719:AAEJrEZgmMiKBjEq6DzyC1CVIUV52a07IEI";
 const PORT = process.env.PORT || 3000;
 const MY_URL = "https://nft-telegram-bot-zrul.onrender.com"; // ✅ ВАШ РЕАЛЬНЫЙ URL!
 const app = express();
